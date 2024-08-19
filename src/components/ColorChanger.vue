a245281c5c44693952ab9af4964552f6d7f2589b
curso14 <curso14@nomail.com>
2024-08-19 10:31:37 +0200

    Proyecto vue una sola página

---
+++ src/components/ColorChanger.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <button :style="{ backgroundColor: buttonColor }" @click="changeColor">Click me</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const buttonColor = ref('red');
+  
+  const changeColor = () => {
+    buttonColor.value = buttonColor.value === 'blue' ? 'green' : 'blue';
+  };
+  </script>
+  
+  <style scoped>
+  button {
+    padding: 0.5rem 1rem;
+    border: none;
+    color: white;
+    cursor: pointer;
+    margin-top: 1rem;
+  }
+  </style>
